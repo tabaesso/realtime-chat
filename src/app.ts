7b4d0d63d09f9cc59be5247c65c2349829dff68c
tabaesso <tatabaesso@gmail.com>
2021-08-31 00:43:40 -0300

    Chat done

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,42 @@
 import 'dotenv/config';
 
-import express, { Request, Response, NextFunction } from 'express';
+import express from 'express';
 import cors from 'cors';
+import path from 'path';
 
 const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'public'));
+
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
 
 app.use(cors());
 app.use(express.json());
 
-app.use((err: any, request: Request, response: Response, _: NextFunction) => {
-  console.error(err);
+const http = require('http').Server(app);
 
-  return response.status(err.statusCode || 500).json({
-    status: 'error',
-    message: err.message,
-  });
+const io = require('socket.io')(http);
+
+app.use('/', (req, res) => {
+  res.render('index.html');
 });
 
-app.listen(3000, () => {
+const messages: any = [];
+
+io.on('connection', (socket: any) => {
+  console.log(`Socket conectado: ${socket.id}`);
+
+  socket.emit('previousMessages', messages);
+
+  socket.on('sendMessage', (message: any) => {
+    messages.push(message);
+
+    socket.broadcast.emit('receivedMessage', message);
+  })
+})
+
+http.listen(3000, () => {
   console.log('.:::::::::. Servidor online .:::::::::.');
 })
